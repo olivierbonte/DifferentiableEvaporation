b68f2233e5008dc015ebb7dac04f6bc155230a82
Olivier Bonte <olivier.bonte@hotmail.com>
2024-07-01 16:08:52 +0200

    update hihydrsoil xee download

--- scripts/hihydrosoil.py
+++ scripts/hihydrosoil.py
@@ -5,33 +5,96 @@ import xarray as xr
 import rioxarray
 import ee  
 import os
+import glob
+import rasterio
+import hvplot.xarray 
+from rasterio.enums import Resampling
 ee.Initialize(opt_url = "https://earthengine-highvolume.googleapis.com")
+
+#%% Define variables of interest from HiHydroSoilv2
+#https://gee-community-catalog.org/projects/hihydro_soil/
+var_list_gee = ["N","wcsat","wcres","wcpf2","wcpf3","wcpf4-2"]
+var_dict = {
+    "N":"n","wcsat":"w_sat","wcres":"w_res",
+    "wcpf2":"w_fc","wcpf3":"w_crit","wcpf4-2":"w_wp"
+} #key = GEE, value = own_name
+full_names_dict = {
+    'n':'N parameter for Mualem Van Genuchten Equation',
+    'w_sat':'Saturated soil moisture',
+    'w_res':'Residual soil moisture',
+    'w_fc':'Soil moisture at field capacity (pF2)',
+    'w_crit':'Soil moisture at critical point (pF3)',
+    'w_wp':'Soil moisture at permanent wilting point (pF4.2)'
+}
+units_dict = {
+    "n":"[-]",
+    "w_sat":"[m^3/m^3]",
+    "w_res":"[m^3/m^3]",
+    "w_fc":"[m^3/m^3]",
+    "w_crit":"[m^3/m^3]",
+    "w_wp":"[m^3/m^3]"
+}
+depth_values = [
+    "0-5cm", "5-15cm", "15-30cm", 
+    "30-60cm", "60-100cm", "100-200cm"
+]
+base_url = "ee://projects/sat-io/open-datasets/HiHydroSoilv2_0/"
 
 #%% Extract to match with data from soilgrids
 sites = ["BE-Bra","ES-LM1"]
 datadir = os.path.join("..","data")
 datarawdir = os.path.join(datadir,"exp_raw")
+soilgrids_dir = os.path.join(datarawdir, "soilgrids")
+hihydrosoil_dir = os.path.join(datarawdir, "hihydrosoil")
+if not os.path.exists(hihydrosoil_dir):
+    os.makedirs(hihydrosoil_dir)
 
 for site in sites:
-    soilgrid_dir = os.path.join(datarawdir,"soilgrids",site)
-    tif_files = [k for k in os.listdir(soilgrid_dir) if ".tif" in k]
-    ds_soilgrids_site = rioxarray.open_rasterio(
-        os.path.join(soilgrid_dir, tif_files[0]), mask_and_scale=True
+    #Get bounding box from soilgrids data
+    soilgrids_file = glob.glob(os.path.join(soilgrids_dir,site,"*" + site + "*.nc"))
+    ds_soilgrids = xr.open_dataset(soilgrids_file[0], decode_coords="all")
+    ds_soilgrids_wgs84 = ds_soilgrids.rio.reproject(
+            dst_crs = "EPSG:4326",
+            resampling = Resampling.bilinear
     )
-    ds_hydrosoil = xr.open_dataset(
-        "ee://projects/sat-io/open-datasets/HiHydroSoilv2_0/wcres",
-        engine = "ee",
-        geometry = tuple(ds_soilgrids_site.rio.bounds()),
-        projection = ee.Projection(
-            ds_soilgrids_site.rio.crs.to_wkt(),
-            #transform = ds_soilgrids_site.rio.transform()[:6]
+    bbox_wgs84 = ds_soilgrids_wgs84.rio.bounds()
+    scale = ds_soilgrids_wgs84.rio.resolution() #spatial spacing in °
+
+    
+    da_list = []
+    for gee_var, var in var_dict.items():
+        #See https://github.com/google/Xee?tab=readme-ov-file#how-to-use
+        ds_temp = xr.open_dataset(
+            base_url + gee_var,
+            engine = "ee",
+            geometry = tuple(bbox_wgs84),
+            projection = ee.Projection(
+                crs = str(ds_soilgrids_wgs84.rio.crs),
+                transform = ds_soilgrids_wgs84.rio.transform()[:6]
+            ),
+            scale = scale
         )
+        ds_temp = ds_temp.rename({list(ds_temp.data_vars.keys())[0]:var})
+        ds_temp[var].attrs.pop("id")
+        ds_temp[var].attrs["GEE_var_name"] = gee_var
+        ds_temp[var].attrs["units"] = units_dict[var]
+        ds_temp[var].attrs["full_name"] = full_names_dict[var]
+        da_list.append(ds_temp[var])
+    #Data cube creation
+    ds_hihydrosoil = xr.merge(da_list)
+    # Convert to correct units by multiplying by 0.0001, see:
+    # https://gee-community-catalog.org/projects/hihydro_soil/
+    ds_hihydrosoil = ds_hihydrosoil * 0.0001
+    # Rename depth dimension
+    ds_hihydrosoil = ds_hihydrosoil.rename({"time":"depth"})
+    ds_hihydrosoil.depth.attrs["Description"] = "Intervals of soil depth below the surface"
+    ds_hihydrosoil = ds_hihydrosoil.assign_coords(depth = depth_values)
+    # Clean up metadata
+    [ds_hihydrosoil.attrs.pop(attr) for attr in ["GEE_var_name","units","full_name"]]
+    ds_hihydrosoil.attrs["url"] = "https://gee-community-catalog.org/projects/hihydro_soil/"
+    ds_hihydrosoil.attrs["url_report"] = "https://www.futurewater.nl/wp-content/uploads/2020/10/HiHydroSoil-v2.0-" \
+                                         "High-Resolution-Soil-Maps-of-Global-Hydraulic-Properties_v2.pdf"
+    # Write to disk as NetCDF
+    ds_hihydrosoil.to_netcdf(
+        os.path.join(hihydrosoil_dir, site + ".nc")
     )
-##IDEA FOR WIP:
-#1.) Get bbox from soilgrids in EPSG:4326
-#2.) get hihydorsoil data in EPSG:4326 at this resolution
-#3.) Convert back to resolution of 
-#LOOK FOR CONSISTENCY WITH LSM PRODUCT!
-
-#%%
-
