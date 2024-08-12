10ddd2cc419affc72965972daa933d4cdf77b4c9
Olivier Bonte <olivier.bonte@hotmail.com>
2024-08-12 16:41:38 +0200

    Remove duplicate folder definition
    Instead of redefening the folders, import their locations from the conf file in the data_download folder.
    Also add new folders to conf file.

--- scripts/data_download/conf.py
+++ scripts/data_download/conf.py
@@ -10,5 +10,8 @@ hihydrosoil_dir = os.path.join(datarawdir, "hihydrosoil")
 ec_dir = os.path.join(datarawdir, "eddy_covariance")
 veg_dir = os.path.join(datarawdir, "vegetation")
 
+soil_pro_dir = os.path.join(datadir, "exp_pro", "soil")
+veg_pro_dir = os.path.join(datadir, "exp_pro", "vegetation")
+
 # %% sites of interest
 sites = ["BE-Bra", "ES-LM1"]
--- scripts/data_processing/soil.py
+++ scripts/data_processing/soil.py
@@ -4,23 +4,25 @@ import os
 import glob
 import xarray as xr
 from rasterio.enums import Resampling
+from importlib.machinery import SourceFileLoader
+from pathlib import Path
 
-datadir = os.path.join("..", "..", "data")
-soilgrids_dir = os.path.join(datadir, "exp_raw", "soilgrids")
-hihydrosoil_dir = os.path.join(datadir, "exp_raw", "hihydrosoil")
-soil_pro_dir = os.path.join(datadir, "exp_pro", "soil")
-if not os.path.exists(soil_pro_dir):
-    os.makedirs(soil_pro_dir)
-sites = os.listdir(soilgrids_dir)
+scripts_folder = Path(__file__).resolve().parent.parent
+conf_module = SourceFileLoader(
+    "conf", os.path.join(scripts_folder, "data_download", "conf.py")
+).load_module()
+if not os.path.exists(conf_module.soil_pro_dir):
+    os.makedirs(conf_module.soil_pro_dir)
 
 # %% Datacube creation
-for site in sites:
+for site in conf_module.sites:
     print(f"site in progress: {site}")
     ds_soilgrids = xr.open_dataset(
-        glob.glob(os.path.join(soilgrids_dir, site, "*.nc"))[0], decode_coords="all"
+        glob.glob(os.path.join(conf_module.soilgrids_dir, site, "*.nc"))[0],
+        decode_coords="all",
     )
     ds_hihydrosoil = xr.open_dataset(
-        os.path.join(hihydrosoil_dir, site + ".nc"), decode_coords="all"
+        os.path.join(conf_module.hihydrosoil_dir, site + ".nc"), decode_coords="all"
     )
 
     ## Convert to 1 cube: (depth, lon, lat) + crs:ESRI_54052
@@ -51,11 +53,18 @@ for site in sites:
     # Add useful metadata
     [ds_soilgrids.attrs.pop(attr) for attr in ["url_doi", "url_OGC"]]
     ds_soil.attrs = ds_soilgrids.attrs
-    ds_soil.to_netcdf(os.path.join(soil_pro_dir, site + "_soil_cube.nc"))
+    ds_soil.to_netcdf(os.path.join(conf_module.soil_pro_dir, site + "_soil_cube.nc"))
 
     # Spatial data aggregation
     ds_soil_agg = ds_soil.mean(dim=["x", "y"], keep_attrs=True)
-    ds_soil_agg.to_netcdf(os.path.join(soil_pro_dir, site + "_soil_horizontal_agg.nc"))
+    ds_soil_agg.to_netcdf(
+        os.path.join(conf_module.soil_pro_dir, site + "_soil_horizontal_agg.nc")
+    )
+
+    # Add vertical aggregation
+    ds_soil_agg_vert = ds_soil_agg.mean(dim=["depth"], keep_attrs=True)
 
     # Aggregate with a weighted mean (respective to its depth)
     # -> Do this in function of the depth of layer 2 we use in the model itself!
+
+# %%
