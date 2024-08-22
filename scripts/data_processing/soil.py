8b17c5715a586dc6cc5128a9ee479825a828e1ca
Olivier Bonte <olivier.bonte@hotmail.com>
2024-08-22 18:17:01 +0200

    Add root depth to soil data processing
    Includes making vertical average up until the root depth weighted by layer depth

--- scripts/data_processing/soil.py
+++ scripts/data_processing/soil.py
@@ -1,28 +1,25 @@
 # %%
-import rioxarray
-import os
 import glob
-import xarray as xr
-from rasterio.enums import Resampling
-from importlib.machinery import SourceFileLoader
-from pathlib import Path
 
-scripts_folder = Path(__file__).resolve().parent.parent
-conf_module = SourceFileLoader(
-    "conf", os.path.join(scripts_folder, "data_download", "conf.py")
-).load_module()
-if not os.path.exists(conf_module.soil_pro_dir):
-    os.makedirs(conf_module.soil_pro_dir)
+import rioxarray
+import xarray as xr
+from conf import conf_module
+from rasterio.enums import Resampling
+
+conf_module.soil_pro_dir.mkdir(exist_ok=True)
 
 # %% Datacube creation
 for site in conf_module.sites:
     print(f"site in progress: {site}")
     ds_soilgrids = xr.open_dataset(
-        glob.glob(os.path.join(conf_module.soilgrids_dir, site, "*.nc"))[0],
+        glob.glob(str(conf_module.soilgrids_dir / site / "*.nc"))[0],
         decode_coords="all",
     )
     ds_hihydrosoil = xr.open_dataset(
-        os.path.join(conf_module.hihydrosoil_dir, site + ".nc"), decode_coords="all"
+        conf_module.hihydrosoil_dir / (site + ".nc"), decode_coords="all"
+    )
+    da_root_depth = xr.open_dataarray(
+        conf_module.essd_dir / "NetCDF" / (site + "_root_depth.nc"), decode_coords="all"
     )
 
     ## Convert to 1 cube: (depth, lon, lat) + crs:ESRI_54052
@@ -48,23 +45,38 @@ for site in conf_module.sites:
         da_tmp = da_tmp.expand_dims({"depth": [var.split("_")[1]]})
         da_list.append(da_tmp)
     ds_soilgrids_depth = xr.merge(da_list)
+    # Assert correct order in depth (as in hihydrosoil)
+    ds_soilgrids_depth = ds_soilgrids_depth.reindex(depth=ds_hihydrosoil.depth)
     # Merge soilgrids and hihydrosoil
     ds_soil = xr.merge([ds_soilgrids_depth, ds_hihydrosoil])
+    # Add root_depth
+    ds_soil["root_depth"] = float(da_root_depth.values)
+    ds_soil["root_depth"].attrs = da_root_depth.attrs
     # Add useful metadata
     [ds_soilgrids.attrs.pop(attr) for attr in ["url_doi", "url_OGC"]]
     ds_soil.attrs = ds_soilgrids.attrs
-    ds_soil.to_netcdf(os.path.join(conf_module.soil_pro_dir, site + "_soil_cube.nc"))
+    ds_soil.to_netcdf(conf_module.soil_pro_dir / (site + "_soil_cube.nc"))
 
-    # Spatial data aggregation
+    # Spatially horizontal data aggregation
     ds_soil_agg = ds_soil.mean(dim=["x", "y"], keep_attrs=True)
-    ds_soil_agg.to_netcdf(
-        os.path.join(conf_module.soil_pro_dir, site + "_soil_horizontal_agg.nc")
+    ds_soil_agg.to_netcdf(conf_module.soil_pro_dir / (site + "_soil_horizontal_agg.nc"))
+
+    # Add vertical aggregation: weighted (on layer depth) mean per layer,
+    # max depth = root_depth
+    # ds_soil_agg_vert = ds_soil_agg.mean(dim=["depth"], keep_attrs=True)
+    # Select layers which are (partly at least) in the available root depth
+    bool_vert = (
+        ds_soil_agg.layer_depth.cumsum() - ds_soil_agg.layer_depth
+    ) < ds_soil_agg.root_depth.values / 100
+    ds_soil_agg_sel = ds_soil_agg.isel(depth=bool_vert)
+    weights = ds_soil_agg_sel.layer_depth
+    # For the deepest layer, the weight is root depth - bottom depth of previous layer!
+    weights[-1] = (
+        ds_soil_agg.root_depth / 100 - ds_soil_agg_sel.layer_depth.cumsum()[-2]
     )
-
-    # Add vertical aggregation
-    ds_soil_agg_vert = ds_soil_agg.mean(dim=["depth"], keep_attrs=True)
-
-    # Aggregate with a weighted mean (respective to its depth)
-    # -> Do this in function of the depth of layer 2 we use in the model itself!
+    # Usage of weights explained here: https://tutorial.xarray.dev/fundamentals/03.4_weighted.html
+    ds_soil_agg_sel_weighted = ds_soil_agg_sel.weighted(weights)
+    ds_soil_agg_vert = ds_soil_agg_sel_weighted.mean(dim="depth", keep_attrs=True)
+    ds_soil_agg_vert.to_netcdf(conf_module.soil_pro_dir / (site + "_total_agg.nc"))
 
 # %%
