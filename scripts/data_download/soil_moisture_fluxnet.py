e81938cf04b4e2ee0312e71ec3c40cd6f2a4d65a
Olivier Bonte <olivier.bonte@hotmail.com>
2024-08-30 17:34:40 +0200

    Include metadata in FLUXNET ICOS download

--- scripts/data_download/soil_moisture_fluxnet.py
+++ scripts/data_download/soil_moisture_fluxnet.py
@@ -6,22 +6,29 @@ from conf import fluxnet_dir, sites
 from icoscp.station import station
 from icoscp_core.icos import auth, data, meta
 
-auth.init_config_file()
+# auth.init_config_file()
 fluxnet_dir.mkdir(exist_ok=True)
 
 # %% Download FLUXNET data for each site
 for site in sites:
     print(f"site in progress: {site}")
     station_tmp = station.get(site)
-    data_type = "http://meta.icos-cp.eu/resources/cpmeta/miscFluxnetArchiveProduct"
+    # All possible datatypes via meta.list_datatypes()
+    data_types = [
+        # "http://meta.icos-cp.eu/resources/cpmeta/miscFluxnetArchiveProduct",
+        "http://meta.icos-cp.eu/resources/cpmeta/etcArchiveProduct",  # MetaData
+        "http://meta.icos-cp.eu/resources/cpmeta/miscFluxnetProduct",  # Fluxnet Half-hourly product
+    ]
     data_list_site = meta.list_data_objects(
-        datatype=data_type, station=station_tmp.info()["uri"]
+        datatype=data_types, station=station_tmp.info()["uri"]
     )
-    if len(data_list_site) > 1:
+    if len(data_list_site) != len(data_types):
         raise Warning(
-            "More than 1 dataset for the combination of dataset and site, which is unwanted"
+            f"""Not the same amount datasets ({len(data_list_site)}) as 
+            dataset types ({len(data_types)}), which is unwanted"""
         )
-    data.save_to_folder(data_list_site[0], str(fluxnet_dir))
+    for data_item in data_list_site:
+        data.save_to_folder(data_item, str(fluxnet_dir))
 
 # %% Unzip the FLUXNET data
 for zip_file in fluxnet_dir.iterdir():
@@ -31,3 +38,5 @@ for zip_file in fluxnet_dir.iterdir():
             extractdir = Path(zip_file.as_posix()[:-4])
             extractdir.mkdir(exist_ok=True)
             zip_ref.extractall(extractdir)
+
+# %%
