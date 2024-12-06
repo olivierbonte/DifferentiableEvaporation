b121c8419f61b0d966ab4c1ccb3738a0d937c94c
Olivier Bonte <olivier.bonte@hotmail.com>
2024-12-06 16:27:52 +0100

    Extend logging info for data download
    Includes log file being written to raw data folder

--- scripts/data_download/main.py
+++ scripts/data_download/main.py
@@ -1,8 +1,34 @@
 import logging
+import os
 import subprocess
+from datetime import datetime
+from pathlib import Path
 
-logging.basicConfig(level=logging.INFO)
+from conf import datarawdir
 
+## Info for logging
+# Get the current time
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# Get the git commit hash
+git_commit = (
+    subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode("utf-8")
+)
+# Get the path of the script
+script_path = os.path.abspath(__file__)
+
+## Logging set up
+log_file = Path(datarawdir / "data_download_log.txt")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    handlers=[logging.FileHandler(log_file), logging.StreamHandler()],
+)
+
+# Log the current time, git commit, and script path
+logging.info(f"Script run at: {current_time}")
+logging.info(f"Git commit: {git_commit}")
+logging.info(f"Script path: {script_path}")
+# Logging of the subprocesses
 logging.info("\n Starting data download script \n")
 logging.info("\n 1: Downloading eddy covaraince data \n")
 subprocess.run("python eddy_covariance.py", shell=True)
