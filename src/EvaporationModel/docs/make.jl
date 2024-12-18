96b803b0876a9ce3f2ca7cb25facc44a9871f387
Olivier Bonte <olivier.bonte@hotmail.com>
2024-12-18 15:41:49 +0100

    Test docs deploy with this branch as dev branch

--- src/EvaporationModel/docs/make.jl
+++ src/EvaporationModel/docs/make.jl
@@ -2,4 +2,7 @@ using Documenter, EvaporationModel
 
 makedocs(; sitename="EvaporationModel", pages=["Home" => "index.md"])
 
-deploydocs(; repo="github.com/olivierbonte/DifferentiableEvaporation.git")
+deploydocs(;
+    repo="github.com/olivierbonte/DifferentiableEvaporation.git",
+    devbranch="feature/online_docs",
+)
