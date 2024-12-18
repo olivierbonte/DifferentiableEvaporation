97a1dca67b23343e38a097324644d547bb926523
Olivier Bonte <olivier.bonte@hotmail.com>
2024-12-18 15:37:42 +0100

    Add deploydocs to document make.jl

--- src/EvaporationModel/docs/make.jl
+++ src/EvaporationModel/docs/make.jl
@@ -1,3 +1,5 @@
 using Documenter, EvaporationModel
 
 makedocs(; sitename="EvaporationModel", pages=["Home" => "index.md"])
+
+deploydocs(; repo="github.com/olivierbonte/DifferentiableEvaporation.git")
