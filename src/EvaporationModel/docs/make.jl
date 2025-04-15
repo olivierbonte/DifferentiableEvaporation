d4240232a960fcf9adf96d07fa233c5e76aa63b3
Olivier Bonte <olivier.bonte@hotmail.com>
2025-04-15 13:50:32 +0200

    Add EvaporationModule to makedocs for testing
    By adding it under modules, doctest defined as `jldoctest` will be run when the documentation is built.
    checkdos=:none for now so that it does not error when not all functions have docmuentation (only temporary)

--- src/EvaporationModel/docs/make.jl
+++ src/EvaporationModel/docs/make.jl
@@ -1,6 +1,11 @@
 using Documenter, EvaporationModel
 
-makedocs(; sitename="EvaporationModel", pages=["Home" => "index.md"])
+makedocs(;
+    sitename="EvaporationModel",
+    pages=["Home" => "index.md"],
+    modules=[EvaporationModel],
+    checkdocs=:none
+)
 
 deploydocs(;
     repo="github.com/olivierbonte/DifferentiableEvaporation.git", push_preview=true
